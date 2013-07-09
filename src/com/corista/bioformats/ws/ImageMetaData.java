27f701afabf94064603759eb4648ea67587fb8b2
Tim McIver <tmciver@Tims-MacBook-Pro.local>
2013-07-09 16:38:23 -0400

    Cleaned up code in ImageMetaData servlet.

--- src/com/corista/bioformats/ws/ImageMetaData.java
+++ src/com/corista/bioformats/ws/ImageMetaData.java
@@ -4,21 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import loci.common.services.DependencyException;
-import loci.common.services.ServiceException;
-import loci.common.services.ServiceFactory;
 import loci.formats.FormatException;
 import loci.formats.IFormatReader;
 import loci.formats.ImageReader;
-import loci.formats.meta.IMetadata;
-import loci.formats.services.OMEXMLService;
-import loci.plugins.in.ImporterOptions;
 
 /**
  * Servlet implementation class ImageMetaData
@@ -30,7 +23,6 @@ public class ImageMetaData extends HttpServlet {
 	private static final String FILENAME_PARAM_NAME = "filename";
 	
 	private String imageDir = "/Users/tmciver/Documents/corista-images/Unsupported/Olympus";
-	private ImporterOptions options;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -50,44 +42,28 @@ public class ImageMetaData extends HttpServlet {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "You must supply a filename parameter value.");
 			return;
 		}
-		
-		// create OME-XML metadata store
-	    ServiceFactory factory = null;
-	    OMEXMLService service = null;
-	    try {
-			factory = new ServiceFactory();
-			service = factory.getInstance(OMEXMLService.class);
-		} catch (DependencyException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
-			return;
-		}
-	    
-	    IMetadata meta;
-		try {
-			meta = service.createOMEXMLMetadata();
-		} catch (ServiceException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
-			return;
-		}
 
 	    // create format reader
 	    IFormatReader reader = new ImageReader();
-	    reader.setMetadataStore(meta);
 	    
 	    // set the ID (image file name)
 	    String imageFile = imageDir + File.separator + filename;
 	    try {
 			reader.setId(imageFile);
 		} catch (FormatException e) {
+			reader.close();
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
 			return;
 		}
-	    
+
 	    // get the width and height
 	    int width = reader.getSizeX();
 	    int height = reader.getSizeY();
 	    
-	    // format the response as JSONJ
+	    // close the reader
+	    reader.close();
+	    
+	    // format the response as JSON
 	    String json = String.format("{ \"width\": \"%d\", \"height\": \"%d\" }", width, height);
 	    
 	    // set up the response
